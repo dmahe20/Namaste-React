1e54bbfa8aca280235e8d145d7cce7e548afd803
mahed <Darshana.Maheshwari@in.pega.com>
2023-10-31 01:32:49 +0530

    adding file in js

--- src/Javascript/RestAndSpreadOperator.js
+++ src/Javascript/RestAndSpreadOperator.js
@@ -1,3 +1,8 @@
+// Rest follows the same syntax as the spread syntax, i.e.,
+//  placing three dots ... before what we choose to copy. 
+//  However, the difference lies in the purpose.
+//   Where spread is used to create copies of arrays/objects, rest is used to collect all the remaining values into an array.
+
 //Rest operator
 //converts list of items into array
 
@@ -6,9 +11,27 @@ function sum(a,b,...restItems){
 }
 sum(1,2,3);
 
+function display(a, b, ...rest) {
+    console.log(a, b, rest);
+ }
+ 
+ display(9,10,11,12,13,14,15,16);
+
 
 //spread operator
 //converts array into list of items
 
 let spreadExample = [1,2,3,4];
 console.log(Math.min(...spreadExample));
+
+//copying array
+function display(arr) {
+    return [...arr, 5,6,7];
+  }
+  const arr = [1,2,3,4];
+  console.log(display(arr)); 
+  
+  //copying object
+  const obj = { name: 'Emily', age: 22 };
+  const newObj = { ...obj }
+  console.log(newObj)
