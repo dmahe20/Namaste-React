de0182b05029107fe917e8f28d24d43e0bb7ebdc
mahed <Darshana.Maheshwari@in.pega.com>
2023-10-31 01:46:40 +0530

    adding file in js

--- src/Javascript/DestructuringArray.js
+++ src/Javascript/DestructuringArray.js
@@ -11,3 +11,17 @@ function display(){
     const [, , ...arr] = list;
     return arr
   } 
+
+  function returnNthCat(n){
+    const state = {
+       cats : [
+          {catId : 1 , name : "tom"},
+          {catId : 2 , name : "tiggy"},
+          {catId : 3 , name : "leo"},
+          {catId : 4 , name : "nixie"}
+       ],
+       curpage : 3
+    }
+        const {cats :{ [n] :{name:thisCatName}}} = state; 
+        return thisCatName;
+    }
