e84b2ffbdb72d64455551d73c29c4b133f7de903
mahed <Darshana.Maheshwari@in.pega.com>
2023-10-31 20:26:06 +0530

    adding file in js

--- src/Javascript/EventPropagation.js
+++ src/Javascript/EventPropagation.js
@@ -18,3 +18,36 @@ document.querySelector('#child').addEventListener('click',
 
 // useCapture = true = capturing
 //  false = bubbling
+
+
+//Another example with color and alert
+//HTMl file
+{/* <div id="wrap"> < div id = "wrap" >
+  <p id="one"> < p id="one" >
+    <a href="#">  < a href="#" > Click Me < / a ></a>
+  < / p >
+  </p>
+< / div >
+</div>
+
+//</div>css
+div, p, a {
+            padding: 20px 30px;
+            display: block;
+        }
+        a{
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+
+        div { background-color: rgb(227, 238, 70); }
+
+        p { background-color: rgb(149, 230, 19); }
+
+        a { background-color: rgb(28, 196, 6); }
+
+//JS
+let elems = document.querySelectorAll('*');
+    elems.forEach(elem => {
+        elem.addEventListener("click", () => alert(`Capturing: ${elem.tagName}`));
+    }); */}
